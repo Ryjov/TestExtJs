8fb3bea4c0e4d5bd73b1582ea5331edabed6d838
Egor Ryzhov <32880315+Ryjov@users.noreply.github.com>
2020-08-31 12:55:09 +0300

    first commit

---
+++ TestExtJs/wwwroot/app.js
@@ -0,0 +1,19 @@
+﻿Ext.application({
+    requires: ['Ext.container.Viewport'],
+    name: 'AM',
+
+    controllers: [
+        'Users'
+    ],
+
+    appFolder: 'extjsapp',
+
+    launch: function () {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: {
+                xtype: 'userlist'
+            }
+        });
+    }
+});
